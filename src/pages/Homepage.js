8f18fcdeb67ebd59d5a674670b02f4bed3f58d30
Sivabalan <sivabalanm205@gmail.com>
2024-10-17 12:56:59 +0530

    demo2

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -15,7 +15,7 @@ const Homepage = () => {
                 <Grid item xs={12} md={6}>
                     <StyledPaper elevation={3}>
                         <StyledTitle>
-                            Welcome to
+                        hello world
                             <br />
                             School Management
                             <br />
